73e047feae4e846741cf6b3c08d90f15eb841433
Erhan Fırat <erhanfirat@gmail.com>
2023-02-23 11:02:50 +0300

    day 4 - axios and promise

--- README.md
+++ README.md
@@ -6,13 +6,14 @@ It is created for the purpose of HTML, CSS, JS course of [workintech](https://wo
 
 ### Week 3
 
-* [Day 1](https://docs.google.com/presentation/d/11ihKcAymRP2hgwxAFiXdOdXCwobYpbrTPNbNbYiPWco/edit?usp=sharing)
-* [Day 2](https://docs.google.com/presentation/d/1q5-2Ppaz2LM3o05vp0g2AV2QZTYq6uVxIQLJm6zqvCs/edit?usp=sharing)
-* [Day 3](https://docs.google.com/presentation/d/1wTc6UipwsM3Xo4fMGdpsYnl9-ndPw1eWh5PbKnJ5ygU/edit?usp=sharing)
-* [Day 4](https://docs.google.com/presentation/d/1Yrv13b8o-HsyKiQGREVsD3irnCKNGZcwiBnCJvmvnt4/edit?usp=sharing)
+- [Day 1](https://docs.google.com/presentation/d/11ihKcAymRP2hgwxAFiXdOdXCwobYpbrTPNbNbYiPWco/edit?usp=sharing)
+- [Day 2](https://docs.google.com/presentation/d/1q5-2Ppaz2LM3o05vp0g2AV2QZTYq6uVxIQLJm6zqvCs/edit?usp=sharing)
+- [Day 3](https://docs.google.com/presentation/d/1wTc6UipwsM3Xo4fMGdpsYnl9-ndPw1eWh5PbKnJ5ygU/edit?usp=sharing)
+- [Day 4](https://docs.google.com/presentation/d/1Yrv13b8o-HsyKiQGREVsD3irnCKNGZcwiBnCJvmvnt4/edit?usp=sharing)
 
 ### Week 5
 
-* [Day 1](https://docs.google.com/presentation/d/1bvjmSJuqoTsO4z4ES9G9SNeFDMLNpub1UUXDikQnbgE/edit?usp=sharing)
-* [Day 2](https://docs.google.com/presentation/d/1NscC0VdS3b7Dqqtr_f4ydQ57uNwoxvYKXQxOxy3ePDg/edit?usp=sharing)
-* [Day 3](https://docs.google.com/presentation/d/1k4GBRKNE0_zI_IcpVpP-Ven3u4r1-a-Hr0jXisplXmo/edit?usp=sharing)
+- [Day 1](https://docs.google.com/presentation/d/1bvjmSJuqoTsO4z4ES9G9SNeFDMLNpub1UUXDikQnbgE/edit?usp=sharing)
+- [Day 2](https://docs.google.com/presentation/d/1NscC0VdS3b7Dqqtr_f4ydQ57uNwoxvYKXQxOxy3ePDg/edit?usp=sharing)
+- [Day 3](https://docs.google.com/presentation/d/1k4GBRKNE0_zI_IcpVpP-Ven3u4r1-a-Hr0jXisplXmo/edit?usp=sharing)
+- [Day 4](https://docs.google.com/presentation/d/1FAiWBLNGzrC_GiGy8nXfKVd8yhOm8PXZDGYQ4ewFBwE/edit?usp=sharing)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,96 +25,96 @@ if (!user) {
   document.getElementsByClassName("user-greeting")[0].prepend(welcome);
 }
 
-// Object Array
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    description: "Ürün açıklaması 1",
-    imgURL: "https://picsum.photos/200/240?random=1",
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    description: "Ürün açıklaması 2",
-    imgURL: "https://picsum.photos/200/240?random=2",
-  },
-  {
-    id: 3,
-    name: "Product 3",
-    description: "Ürün açıklaması 3",
-    imgURL: "https://picsum.photos/200/240?random=3",
-  },
-  {
-    id: 4,
-    name: "Product 4",
-    description: "Ürün açıklaması 4",
-    imgURL: "https://picsum.photos/200/240?random=4",
-  },
-  {
-    id: 5,
-    name: "Product 5",
-    description: "Ürün açıklaması 5",
-    imgURL: "https://picsum.photos/200/240?random=5",
-  },
-  {
-    id: 6,
-    name: "Product 6",
-    description: "Ürün açıklaması 6",
-    imgURL: "https://picsum.photos/200/240?random=6",
-  },
+// // Object Array
+// const products = [
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     description: "Ürün açıklaması 1",
+//     imgURL: "https://picsum.photos/200/240?random=1",
+//   },
+//   {
+//     id: 2,
+//     name: "Product 2",
+//     description: "Ürün açıklaması 2",
+//     imgURL: "https://picsum.photos/200/240?random=2",
+//   },
+//   {
+//     id: 3,
+//     name: "Product 3",
+//     description: "Ürün açıklaması 3",
+//     imgURL: "https://picsum.photos/200/240?random=3",
+//   },
+//   {
+//     id: 4,
+//     name: "Product 4",
+//     description: "Ürün açıklaması 4",
+//     imgURL: "https://picsum.photos/200/240?random=4",
+//   },
+//   {
+//     id: 5,
+//     name: "Product 5",
+//     description: "Ürün açıklaması 5",
+//     imgURL: "https://picsum.photos/200/240?random=5",
+//   },
+//   {
+//     id: 6,
+//     name: "Product 6",
+//     description: "Ürün açıklaması 6",
+//     imgURL: "https://picsum.photos/200/240?random=6",
+//   },
 
-  {
-    id: 7,
-    name: "Product 7",
-    description: "Ürün açıklaması 7",
-    imgURL: "https://picsum.photos/200/240?random=7",
-  },
+//   {
+//     id: 7,
+//     name: "Product 7",
+//     description: "Ürün açıklaması 7",
+//     imgURL: "https://picsum.photos/200/240?random=7",
+//   },
 
-  {
-    id: 8,
-    name: "Product 8",
-    description: "Ürün açıklaması 8",
-    imgURL: "https://picsum.photos/200/240?random=8",
-  },
+//   {
+//     id: 8,
+//     name: "Product 8",
+//     description: "Ürün açıklaması 8",
+//     imgURL: "https://picsum.photos/200/240?random=8",
+//   },
 
-  {
-    id: 9,
-    name: "Product 9",
-    description: "Ürün açıklaması 9",
-    imgURL: "https://picsum.photos/200/240?random=9",
-  },
+//   {
+//     id: 9,
+//     name: "Product 9",
+//     description: "Ürün açıklaması 9",
+//     imgURL: "https://picsum.photos/200/240?random=9",
+//   },
 
-  {
-    id: 10,
-    name: "Product 10",
-    description: "Ürün açıklaması 10",
-    imgURL: "https://picsum.photos/200/240?random=10",
-  },
+//   {
+//     id: 10,
+//     name: "Product 10",
+//     description: "Ürün açıklaması 10",
+//     imgURL: "https://picsum.photos/200/240?random=10",
+//   },
 
-  {
-    id: 11,
-    name: "Product 11",
-    description: "Ürün açıklaması 11",
-    imgURL: "https://picsum.photos/200/240?random=11",
-  },
+//   {
+//     id: 11,
+//     name: "Product 11",
+//     description: "Ürün açıklaması 11",
+//     imgURL: "https://picsum.photos/200/240?random=11",
+//   },
 
-  {
-    id: 12,
-    name: "Product 12",
-    description: "Ürün açıklaması 12",
-    imgURL: "https://picsum.photos/200/240?random=12",
-  },
-];
+//   {
+//     id: 12,
+//     name: "Product 12",
+//     description: "Ürün açıklaması 12",
+//     imgURL: "https://picsum.photos/200/240?random=12",
+//   },
+// ];
 
-const appendProductsIntoContainer = () => {
+const appendProductsIntoContainer = (products) => {
   const productsContainer = document.getElementById(
     "products-container-section"
   );
 
   for (const product of products) {
     const productItem = generateProductItem(
-      product.imgURL,
+      product.img,
       product.name,
       product.description
     );
@@ -122,8 +122,6 @@ const appendProductsIntoContainer = () => {
     productsContainer.append(productItem);
   }
 };
-
-appendProductsIntoContainer();
 
 const productCards = document.querySelectorAll(".product-item");
 
@@ -164,3 +162,52 @@ const sepetBtn = generateIconButton("cart-shopping", "Sepete Git", () => {
 sepetBtn.append(document.createElement("span"));
 
 document.getElementById("user-info-panel").append(sepetBtn);
+
+// const sifirdanYuzeKadarCiftSayiBul = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log("düşünüyoruummm…");
+//     setTimeout(() => {
+//       const sayi = Math.floor(Math.random() * 100);
+//       if (sayi % 2 === 0) {
+//         resolve(sayi);
+//       } else {
+//         reject(sayi);
+//       }
+//     }, 3000);
+//   });
+// };
+
+// // const sayiBulmaPromise = sifirdanYuzeKadarCiftSayiBul();
+
+// // console.log("sayiBulmaPromise > ", sayiBulmaPromise);
+
+// sifirdanYuzeKadarCiftSayiBul()
+//   .then((sayi) => {
+//     console.log("OLEY > ", sayi);
+//   })
+//   .catch((sayi) => {
+//     console.log("Ama bu çift değil! ", sayi);
+//   })
+//   .finally(() => {
+//     console.log("Teşekkürler..");
+//   });
+
+axios
+  .get("https://620d69fb20ac3a4eedc05e3a.mockapi.io/api/products")
+  .then((res) => {
+    console.log("res from server > ", res);
+    console.log("data from server > ", res.data);
+    appendProductsIntoContainer(res.data);
+  })
+  .catch((err) => {
+    console.error("Oops there is an error > ", err);
+  })
+  .finally(() => {
+    console.log("HTTP request is completed!");
+  });
+
+// axios.post("https://620d69fb20ac3a4eedc05e3a.mockapi.io/api/products", {
+//   name: "Yeni product",
+//   description: "Bu productı axios post requesti ile oluşturdum!",
+//   img: "https://picsum.photos/200/240?random=123123",
+// });
